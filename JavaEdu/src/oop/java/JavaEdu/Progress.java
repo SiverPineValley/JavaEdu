2eb352dbe111f041a33b9e1841c0c993e763586f
ajou <ajou@ajou-PC>
2017-06-12 17:58:32 +0900

    창 안닫아지게

--- JavaEdu/src/oop/java/JavaEdu/Progress.java
+++ JavaEdu/src/oop/java/JavaEdu/Progress.java
@@ -21,7 +21,6 @@ public class Progress extends JFrame {
 	 * Create the frame.
 	 */
 	public Progress(User usr, Chapter chpt, int lastpage) {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
--- JavaEdu/src/oop/java/JavaEdu/Qz/quizmenu.java
+++ JavaEdu/src/oop/java/JavaEdu/Qz/quizmenu.java
@@ -32,7 +32,6 @@ public class quizmenu extends Quiz {
 
 
 	public quizmenu(String s) {
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
